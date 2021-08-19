4d9f2745b58e108a325078cb08d883d652f78a69
voncardoso <vonharrison26@gmail.com>
2021-08-19 17:31:39 -0300

    função de fechar menu hamburger

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 const btnMobile = document.querySelector('.btn-mobile');
+const fechabtn = document.querySelector('.sobreBtn');
+const fechabtn1 = document.querySelector('.sobreBtn1');
+const fechabtn2 = document.querySelector('.sobreBtn2');
 
 function toggleMenu(){
     const nav = document.querySelector('.menu-nav');
     nav.classList.toggle('active');
 }
 
+function FechaMenu(){
+    const nav1 = document.querySelector('.menu-nav');
+    nav1.classList.toggle('active');
+}
+
 btnMobile.addEventListener('click', toggleMenu);
+
+fechabtn.addEventListener('click', FechaMenu);
+fechabtn1.addEventListener('click', FechaMenu);
+fechabtn2.addEventListener('click', FechaMenu);
